e1b9c863a457082d7633e0c931d81c67f46aae07
Bijon Krishna Bairagi <bijon.bairagi@gmail.com>
2024-11-04 22:43:51 +0600

    order tabs add

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -4,12 +4,19 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Filament\Resources\OrderResource\RelationManagers\AddressRelationManager;
 use App\Models\Order;
+use App\Models\Product;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -17,6 +24,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Number;
 
 class OrderResource extends Resource
 {
@@ -67,8 +75,91 @@ class OrderResource extends Resource
                                     'deliverd' => 'success',
                                     'cancelled' => 'danger'
                                 ])
+                            ->icons([
+                                'new' => 'heroicon-m-sparkles',
+                                'processing' => 'heroicon-m-arrow-path',
+                                'shipped' => 'heroicon-m-truck',
+                                'deliverd' => 'heroicon-m-check-badge',
+                                'cancelled' => 'heroicon-m-x-circle'
+                            ]),
+                        Select::make('currency')
+                            ->options([
+                                'bdt' => 'BDT',
+                                'inr' => 'INR',
+                                'usd' => 'USD',
+                                'eur' => 'EUR',
+                                'gbp' => 'GBP'
+                            ])
+                            ->default('bdt')
+                            ->required(),
+                        Select::make('shipping_method')
+                            ->options([
+                                'fedex' => 'FedEx',
+                                'ups' => 'UPS',
+                                'dhl' => 'DHL',
+                                'usps' => 'USPS'
+                            ])->required(),
+
+                        Textarea::make('notes')
+                            ->columnSpanFull()
+                    ])->columns(2),
+                    Section::make('Order Items')->schema([
+                        Repeater::make('items')
+                            ->relationship()
+                            ->schema([
+                                Select::make('product_id')
+                                    ->relationship('product', 'name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->required()
+                                    ->distinct()
+                                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                                    ->reactive()
+                                    ->afterStateUpdated(fn($state, $set) => $set('unit_amount', Product::find($state)?->price ?? 0))
+                                    ->afterStateUpdated(fn($state, $set) => $set('total_amount', Product::find($state)?->price ?? 0))
+                                    ->columnSpan(4),
+
+                                TextInput::make('quantaty')
+                                    ->numeric()
+                                    ->required()
+                                    ->default(1)
+                                    ->minValue(1)
+                                    ->columnSpan(2)
+                                    ->reactive()
+                                    ->afterStateUpdated(fn($state, $set, $get) => $set('total_amount', $state * $get('unit_amount')))
+                                ,
+
+                                TextInput::make('unit_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->columnSpan(3),
+
+                                TextInput::make('total_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->columnSpan(3)
+                            ])->columns(12),
+                        Placeholder::make('grand_total_placeholder')
+                            ->label('Grand Total')
+                            ->content(function ($get, $set) {
+                                $total = 0;
+                                if (!$repeaters = $get('items')) {
+                                    return $total;
+                                }
+
+                                foreach ($repeaters as $key => $repeater) {
+                                    $total += $get("items.{$key}.total_amount");
+                                }
+                                $set('grand_total', $total);
+                                return Number::currency($total, 'Tk.');
+                            }),
+                        Hidden::make('grand_total')->default(0)
                     ])
-                ])
+                ])->columnSpanFull()
                 // Forms\Components\TextInput::make('user_id')
                 //     ->required()
                 //     ->numeric(),
@@ -95,9 +186,11 @@ class OrderResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('user_id')
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Customer')
                     ->numeric()
-                    ->sortable(),
+                    ->sortable()
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('grand_total')
                     ->numeric()
                     ->sortable(),
@@ -139,8 +232,18 @@ class OrderResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            AddressRelationManager::class
         ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+    public static function getNavigationBadgeColor(): string|array|null
+    {
+        return static::getModel()::count() > 10 ? 'info' : 'success';
     }
 
     public static function getPages(): array
--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListOrders extends ListRecords
 {
@@ -14,6 +17,24 @@ class ListOrders extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class
+        ];
+    }
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'new' => Tab::make()->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'new')),
+            'processing' => Tab::make()->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'processing')),
+            'shipped' => Tab::make()->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'shipped')),
+            'deliverd' => Tab::make()->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'deliverd')),
+            'cancelled' => Tab::make()->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'cancelled')),
         ];
     }
 }
